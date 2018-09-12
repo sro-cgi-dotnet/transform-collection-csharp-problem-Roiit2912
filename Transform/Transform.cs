825f2ad7bbd2ed05344d1db371fa742a76a91b46
Roiit2912 <rohit.rathi2912@gmail.com>
2018-09-12 16:31:47 +0530

    assignment4 added

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -2,13 +2,20 @@
 using System.Linq;
 using System.Collections.Generic;
 
+public delegate int done (int i);
+
 namespace Transform
 {
     public static class Transform 
     {
-        public static void Map()
+        public static int[] Map(this int [] a, done fun)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = fun(a[i]);
+            }
+            return a;
         }
     }
 }
